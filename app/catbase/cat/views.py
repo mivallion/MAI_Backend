b9e8ba44359aaabf75e75fcddcbb6871b0db1025
mivallion <mivallion@gmail.com>
2021-11-01 14:35:34 +0300

    Обработка только нужных запросов (GET, POST). Пример простого приложения на gunicorn.

--- app/catbase/cat/views.py
+++ app/catbase/cat/views.py
@@ -1,38 +1,49 @@
 from django.http import JsonResponse
 import utils.cache.lru_cache as lru_cache
 
+from django.views.decorators.http import require_http_methods
 
+
+@require_http_methods(["GET"])
 def read_user(request, user_id):
     return JsonResponse({"id": user_id})
 
 
+@require_http_methods(["POST"])
 def create_cat(request):
     return JsonResponse({"id": 1})
 
 
+@require_http_methods(["POST"])
 def update_cat(request, cat_id):
     return JsonResponse({"id": cat_id})
 
 
+@require_http_methods(["GET"])
 def read_cat(request, cat_id):
     return JsonResponse({"id": cat_id})
 
 
+@require_http_methods(["POST"])
 def delete_cat(request, cat_id):
     return JsonResponse({"id": cat_id})
 
 
+@require_http_methods(["POST"])
 def create_review(request):
     return JsonResponse({"id": 1})
 
 
+@require_http_methods(["POST"])
 def update_review(request, review_id):
     return JsonResponse({"id": review_id})
 
 
+@require_http_methods(["GET"])
 def read_review(request, review_id):
     return JsonResponse({"id": review_id})
 
 
+@require_http_methods(["POST"])
 def delete_review(request, review_id):
     return JsonResponse({"id": review_id})
